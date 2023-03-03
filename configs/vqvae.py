dbb9ad3c5aafc5d68a98198122c72ac332ad918b
egor <radiation7@protonmail.com>
2023-03-03 17:30:22 +0100

    Multi-level VQVAE

--- configs/vqvae.py
+++ configs/vqvae.py
@@ -12,10 +12,18 @@ def get_config():
     # VAE architecture parameters
     config.in_features = 41
     config.hidden_features = 32
-    config.embed_dim = 16
+
+    config.embed_dim1 = 2
+    config.embed_entries1 = 16
+
+    config.embed_dim2 = 4
+    config.embed_entries2 = 16
+
+    config.embed_dim3 = 5
+    config.embed_entries3 = 32
+
     config.n_layers = 3
     config.kld_scale = 5e-4
-    config.nb_entries = 256
     config.temperature = 1
     config.straight_through = False
 
--- models/vqvae.py
+++ models/vqvae.py
@@ -7,12 +7,12 @@ from .types_ import *
 
 
 class ReZero(torch.nn.Module):
-    def __init__(self, config: config_dict.ConfigDict) -> None:
+    def __init__(self, hidden_dim: int) -> None:
         super(ReZero, self).__init__()
-
+        self.hidden_dim = hidden_dim
         self.layers = nn.Sequential(
-            nn.Linear(config.hidden_features, config.hidden_features),
-            nn.BatchNorm1d(config.hidden_features),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.BatchNorm1d(hidden_dim),
             nn.ReLU(inplace=True),
         )
         self.alpha = nn.Parameter(torch.tensor(0.0))
@@ -22,24 +22,27 @@ class ReZero(torch.nn.Module):
 
 
 class ResidualStack(torch.nn.Module):
-    def __init__(self, config: config_dict.ConfigDict) -> None:
+    def __init__(self, hidden_dim: int, n_layers: int) -> None:
         super(ResidualStack, self).__init__()
-
-        self.stack = nn.Sequential(*[ReZero(config) for _ in range(config.n_layers)])
+        self.hidden_dim = hidden_dim
+        self.stack = nn.Sequential(*[ReZero(hidden_dim) for _ in range(n_layers)])
 
     def forward(self, x: torch.FloatTensor) -> torch.FloatTensor:
         return self.stack(x)
 
 
 class Encoder(torch.nn.Module):
-    def __init__(self, config: config_dict.ConfigDict) -> None:
+    def __init__(self, input_dim: int, hidden_dim: int, n_layers:int) -> None:
         super(Encoder, self).__init__()
 
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
         layers = []
-        if config.in_features != config.hidden_features:
-            layers.append(nn.Linear(config.in_features, config.hidden_features))
+        if self.input_dim != self.hidden_dim:
+            layers.append(nn.Linear(self.input_dim, self.hidden_dim))
 
-        layers.append(ResidualStack(config))
+        layers.append(ResidualStack(hidden_dim=self.hidden_dim, n_layers=self.n_layers))
 
         self.layers = nn.Sequential(*layers)
 
@@ -48,17 +51,22 @@ class Encoder(torch.nn.Module):
 
 
 class Decoder(torch.nn.Module):
-    def __init__(self, config: config_dict.ConfigDict) -> None:
+    def __init__(self, input_dim: int, hidden_dim: int, output_dim: int, n_layers:int) -> None:
         super(Decoder, self).__init__()
 
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+        self.n_layers = n_layers
+
         layers = []
-        if config.embed_dim != config.hidden_features:
-            layers.append(nn.Linear(config.embed_dim, config.hidden_features))
+        if self.input_dim != self.hidden_dim:
+            layers.append(nn.Linear(self.input_dim, self.hidden_dim))
 
-        layers.append(ResidualStack(config))
+        layers.append(ResidualStack(hidden_dim=self.hidden_dim, n_layers=self.n_layers))
 
-        if config.in_features != config.hidden_features:
-            layers.append(nn.Linear(config.hidden_features, config.in_features))
+        if self.output_dim != self.hidden_dim:
+            layers.append(nn.Linear(self.hidden_dim, self.output_dim))
 
         self.layers = nn.Sequential(*layers)
 
@@ -67,20 +75,28 @@ class Decoder(torch.nn.Module):
 
 
 class CodeLayer(torch.nn.Module):
-    def __init__(self, config: config_dict.ConfigDict) -> None:
+    def __init__(self,
+                 in_features: int,
+                 embed_dim: int,
+                 embed_entries: int,
+                 temperature: float,
+                 kld_scale: float,
+                 straight_through: bool) -> None:
         super(CodeLayer, self).__init__()
-        if config.hidden_features != config.embed_dim:
-            self.linear_in = nn.Linear(config.hidden_features, config.nb_entries)
+
+        self.in_features = in_features
+        self.embed_dim = embed_dim
+        self.embed_entries = embed_entries
+        self.straight_through = straight_through
+        self.temperature = temperature
+        self.kld_scale = kld_scale
+
+        if self.in_features != self.embed_dim:
+            self.linear_in = nn.Linear(self.in_features, self.embed_entries)
         else:
             self.linear_in = nn.Identity()
 
-        self.embed_dim = config.embed_dim
-        self.nb_entries = config.nb_entries
-        self.straight_through = config.straight_through
-        self.temperature = config.temperature
-        self.kld_scale = config.kld_scale
-
-        embed = torch.randn(self.nb_entries, self.embed_dim, dtype=torch.float32)
+        embed = torch.randn(self.embed_entries, self.embed_dim, dtype=torch.float32)
         self.register_buffer("embed", embed)
 
     @torch.cuda.amp.autocast(enabled=False)
@@ -94,7 +110,7 @@ class CodeLayer(torch.nn.Module):
 
         # + kl divergence to the prior loss
         qy = F.softmax(x, dim=1)
-        diff = torch.sum(qy * torch.log(qy * self.nb_entries + 1e-10), dim=1).mean()
+        diff = torch.sum(qy * torch.log(qy * self.embed_entries + 1e-10), dim=1).mean()
 
         embed_ind = soft_one_hot.argmax(dim=1)
 
@@ -106,23 +122,59 @@ class VQVAE(BaseVAE):
     def __init__(self, config: config_dict.ConfigDict) -> None:
         super(VQVAE, self).__init__()
 
-        self.encoder = Encoder(config)
-        self.codebook = CodeLayer(config)
-        self.decoder = Decoder(config)
+        self.encoder = Encoder(input_dim=config.in_features, hidden_dim=config.hidden_features, n_layers=config.n_layers)
+        self.codebook1 = CodeLayer(in_features=config.hidden_features,
+                                   embed_dim=config.embed_dim1,
+                                   embed_entries=config.embed_entries1,
+                                   temperature=config.temperature,
+                                   kld_scale=config.kld_scale,
+                                   straight_through=config.straight_through)
+        self.codebook2 = CodeLayer(in_features=config.hidden_features+config.in_features,
+                                   embed_dim=config.embed_dim2,
+                                   embed_entries=config.embed_entries2,
+                                   temperature=config.temperature,
+                                   kld_scale=config.kld_scale,
+                                   straight_through=config.straight_through)
+        self.codebook3 = CodeLayer(in_features=config.hidden_features+config.in_features,
+                                   embed_dim=config.embed_dim3,
+                                   embed_entries=config.embed_entries3,
+                                   temperature=config.temperature,
+                                   kld_scale=config.kld_scale,
+                                   straight_through=config.straight_through)
 
+        self.decoder1 = Decoder(input_dim=config.embed_dim1,
+                                hidden_dim=config.hidden_features,
+                                output_dim=config.in_features,
+                                n_layers=config.n_layers)
+
+        self.decoder2 = Decoder(input_dim=config.embed_dim2+config.embed_dim1,
+                                hidden_dim=config.hidden_features,
+                                output_dim=config.in_features,
+                                n_layers=config.n_layers)
+
+        self.decoder3 = Decoder(input_dim=config.embed_dim3+config.embed_dim2,
+                                hidden_dim=config.hidden_features,
+                                output_dim=config.in_features,
+                                n_layers=config.n_layers)
         self.kld_scale = config.kld_scale
 
     def forward(self, x):
         encoder_output = self.encoder(x)
-        code_q, code_d, emb_id = self.codebook(encoder_output)
-        decoder_output = self.decoder(code_q)
+        code_q1, code_d1, emb_id1 = self.codebook1(encoder_output)
+        decoder_output1 = self.decoder1(code_q1)
 
-        return decoder_output, x, code_d, encoder_output, emb_id
+        code_q2, code_d2, emb_id2 = self.codebook2(torch.cat([encoder_output, decoder_output1], axis=1))
+        decoder_output2 = self.decoder2(torch.cat([code_q2, code_q1], axis=1))
+
+        code_q3, code_d3, emb_id3 = self.codebook3(torch.cat([encoder_output, decoder_output2], axis=1))
+        decoder_output3 = self.decoder3(torch.cat([code_q3, code_q2], axis=1))
+
+        return decoder_output3, x, [code_d1,code_d2,code_d3], encoder_output, [emb_id1, emb_id2, emb_id3]
 
     def loss_function(self, *args) -> dict:
         recons = args[0]
         input = args[1]
-        l_loss = args[2]
+        l_loss = sum(args[2])
         r_loss = recons.sub(input).pow(2).mean()
         loss = r_loss + self.kld_scale * l_loss
         return {'loss': loss, 'MSE': r_loss.detach(), 'KLD': l_loss.detach()}
